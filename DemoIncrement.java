746318e3c132897eddc4a6bbfcd0c1e62c352edd
BellaStock <is557@exeter.ac.uk>
2026-01-31 09:16:58 +0000

    DemoIncrement completed

--- DemoIncrement.java
+++ DemoIncrement.java
@@ -1,3 +1,35 @@
 // Add your code below
 
+/* Predictions:
+a=3
+b=8
+x=3
+y=8 */
+
+public class DemoIncrement {
+        public static void main(String[] args) {
+                // Demonstration of the increment operator
+                        int a = 3;
+                        int b = a++ + 5;
+                        int x = 3;
+                        int y = ++x + 5;
+                        int p = 3;
+                        p++;
+                        int q = p + 5;
+            
+
+        System.out.print("a: " + a);
+        System.out.print(", b: " + b);
+        System.out.print(", x: " + x);
+        System.out.print(", y: " + y);
+        System.out.print(", p: " + p);
+        System.out.println(", q: " + q);
+    
+
+        // Replace the xxxxx in the following with the correct term
+        System.out.println("i++ means pre-increment");
+        System.out.println("++i means post-increment");
+
+    }
+}
 
